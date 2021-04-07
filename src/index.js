5a8a9402e1c4c3a6362ea60d1996ce691a1b936e
AnaBetancourt <anaa.betancourt13@gmail.com>
2021-04-06 21:03:25 -0400

    commit tree 0916ac9809a9be0ddeff13c2810a572a5d0bab6c

--- src/index.js
+++ src/index.js
@@ -1,1 +1,34 @@
-console.log('%c HI', 'color: firebrick')
+
+const breedUrl = 'https://dog.ceo/api/breeds/list/all' 
+
+function fetchImages(){
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4" 
+    fetch(imgUrl)
+    .then(resp => resp.json())
+    .then(json => addImages(json))
+}
+
+function addImages(json){
+    const imagesContainer = document.getElementById('dog-image-container')
+    const imgArray = json.message
+    imgArray.forEach(function(element){
+        let img = document.createElement('img')
+        img.src = element
+        // debugger
+        imagesContainer.innerHTML += img
+    })
+}
+
+function fetchBreeds(){
+    fetch(breedUrl)
+    .then(resp => resp.json())
+    .then(json => addBreeds(json))
+}
+
+function addBreeds(json){
+    const breedList = document.getElementById('dog-breeds')
+    console.log(json.message)
+}
+
+document.addEventListener("DOMContentLoaded", fetchImages())
+document.addEventListener("DOMContentLoaded", fetchBreeds())
